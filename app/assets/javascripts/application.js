44f4b62000bddc7341e69ac2b9bc0c92575a23d9
Yin Long <blue.rose.hut@gmail.com>
2016-05-03 09:52:51 +0700

    Add css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,17 @@
 //= require jquery_ujs
 //= require angular-rails-templates
 //= require_tree ../templates
-
+//= require nprogress
+//= require nprogress-turbolinks
+//= require dataTables/jquery.dataTables
 //= require_tree .
 
+$(document).ready(function() {
+
+  var cb = function(start, end, label) {
+    console.log(start.toISOString(), end.toISOString(), label);
+    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    //alert("Callback has fired: [" + start.format('MMMM D, YYYY') + " to " + end.format('MMMM D, YYYY') + ", label = " + label + "]");
+  }
+  NProgress.done();
+});
